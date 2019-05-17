dceae1696ce950bec80bb3461e2f38167ac3d56d
Johnson Denen <johnson.denen@gmail.com>
2019-05-17 12:40:40 -0400

    Setup presto

--- setup.sh
+++ setup.sh
@@ -4,24 +4,21 @@
 # helm init
 # eval $(minikube docker-env)
 # cd ~/code
-
-PLATFORM_IP=$(minikube ip)
+# git clone https://github.com/dharmeshkakadia/presto-kubernetes/
 
 echo --------------
 echo REDIS
 echo --------------
-helm install --name redis stable/redis --set usePassword=false,cluster.enabled=false --namespace redis
+helm upgrade --install redis stable/redis --set usePassword=false,cluster.enabled=false --namespace redis
 
 echo --------------
 echo ANDI
 echo --------------
-
 cd andi
-docker build -t andi:demo .
-helm upgrade --install andi ./chart --set image.repository=andi,image.tag=demo,replicaCount=1,redis.host=redis-master.redis --namespace admin
+# docker build -t andi:demo .
+helm upgrade --install andi ./chart --namespace admin \
+     --set image.repository=andi,image.tag=demo,replicaCount=1,redis.host=redis-master.redis
 cd ..
-
-ANDI_PORT=$(kubectl get svc -n admin | tail -1 | awk '{print $5}' | cut -d: -f2 | cut -d/ -f1)
 
 echo --------------
 echo KAFKA
@@ -34,19 +31,24 @@ echo --------------
 echo --------------
 echo PRESTO
 echo --------------
+kubectl create ns kdp
+kubectl create -n kdp -f presto-kubernetes
 
 echo --------------
 echo CARPENTER
 echo --------------
+cd carpenter
+cd ..
 
 echo --------------
 echo FORKLIFT
 echo --------------
+cd forklift
+cd ..
 
 echo --------------
 echo DISCOVERY-API
 echo --------------
-
 # cd discovery-api
 # docker build -t api:demo .
 # cd ..
@@ -54,15 +56,21 @@ echo --------------
 echo --------------
 echo GENESIS
 echo --------------
+cd genesis
+cd ..
 
 echo --------------
 echo STREISAND
 echo --------------
+cd streisand
+cd ..
 
 echo --------------
 echo DISCOVERY-STREAMS
 echo --------------
+cd discovery-streams
+cd ..
 
 echo --------------
-echo ANDI: http://${PLATFORM_IP}:${ANDI_PORT}
+echo ANDI: $(minikube service andi -n admin --url)
 echo --------------

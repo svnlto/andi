aced796e51c968b823141d81a5aec7052f788bdf
Johnson Denen <johnson.denen@gmail.com>
2019-05-16 20:20:32 -0400

    Andi up and working with Redis

--- setup.sh
+++ setup.sh
@@ -1,13 +1,68 @@
 #!/usr/bin/env bash
 
-eval $(minikube docker-env)
+# minikube start --memory 14336 --cpus 4
+# helm init
+# eval $(minikube docker-env)
+# cd ~/code
+
+PLATFORM_IP=$(minikube ip)
+
+echo --------------
+echo REDIS
+echo --------------
+helm install --name redis stable/redis --set usePassword=false,cluster.enabled=false --namespace redis
+
+echo --------------
+echo ANDI
+echo --------------
 
 cd andi
 docker build -t andi:demo .
-helm upgrade --install andi ./chart --set image.repository=andi,image.tag=demo,replicaCount=1 --namespace admin
+helm upgrade --install andi ./chart --set image.repository=andi,image.tag=demo,replicaCount=1,redis.host=redis-master.redis --namespace admin
 cd ..
 
-cd discovery-api
-docker build -t api:demo .
-# TODO helm
-cd ..
+ANDI_PORT=$(kubectl get svc -n admin | tail -1 | awk '{print $5}' | cut -d: -f2 | cut -d/ -f1)
+
+echo --------------
+echo KAFKA
+echo --------------
+
+echo --------------
+echo REAPER
+echo --------------
+
+echo --------------
+echo PRESTO
+echo --------------
+
+echo --------------
+echo CARPENTER
+echo --------------
+
+echo --------------
+echo FORKLIFT
+echo --------------
+
+echo --------------
+echo DISCOVERY-API
+echo --------------
+
+# cd discovery-api
+# docker build -t api:demo .
+# cd ..
+
+echo --------------
+echo GENESIS
+echo --------------
+
+echo --------------
+echo STREISAND
+echo --------------
+
+echo --------------
+echo DISCOVERY-STREAMS
+echo --------------
+
+echo --------------
+echo ANDI: http://${PLATFORM_IP}:${ANDI_PORT}
+echo --------------
